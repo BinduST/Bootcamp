60058818c457f487aff1bfa98f53c598ecb057be
Bindu S <bindus@thoughtworks.com>
2016-03-18 10:49:45 +0530

    Implemented probabilityGenerator and Chance for single and multiple events.

--- src/chance/Chance.java
+++ src/chance/Chance.java
@@ -1,19 +1,40 @@
 
 /*
     Job:-
-        Calculate the probability to get a face after flipping coins
+        Given the probability class should be able to give chance of occurrence of the event(s)
  */
 package chance;
 
 public class Chance {
-    public double getChanceToGetTail(double noOfCoins) {
-        double chances = Math.pow(noOfCoins,2);
-        if (noOfCoins == 1.0)
-            return 0.5;
-        return 1.0 / chances;
+    private double value;
+
+    private Chance(double value) {
+        this.value = value;
     }
 
-    public double getChanceToNotGetTail(double noOfCoins) {
-        return (1 - getChanceToGetTail(noOfCoins));
+
+    public static Chance create(double value) {
+        if (value < 0 || value > 1)
+            throw new IllegalArgumentException("The value should be between 0 and 1, but actual is " + value);
+        return new Chance(value);
+    }
+
+    @Override
+    public boolean equals(Object otherChance) {
+        if (this == otherChance) return true;
+        if (otherChance == null || getClass() != otherChance.getClass()) return false;
+
+        Chance chance = (Chance) otherChance;
+        return Double.compare(chance.value, value) == 0;
+
+    }
+
+    public Chance not() {
+        return new Chance(1 - value);
+    }
+
+    public Chance multiply(Chance otherChance) {
+        double product = otherChance.value * value;
+        return Chance.create(product);
     }
 }
--- test/chance/ChanceTest.java
+++ test/chance/ChanceTest.java
@@ -1,38 +1,18 @@
 package chance;
 
-import org.junit.Assert;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 public class ChanceTest {
-    @Test
-    public void getChanceToGetTail_should_return_the_probability_to_get_tail() {
-        Chance coinToss = new Chance();
-        int noOfCoins = 1;
-        double probability = coinToss.getChanceToGetTail(noOfCoins);
-        Assert.assertEquals(0.5, probability, 0.0);
-    }
+    @Rule
+    public ExpectedException thrown  = ExpectedException.none();
 
     @Test
-    public void getChanceTNotoGetTail_should_return_the_probability_to_get_tail() {
-        Chance coinToss = new Chance();
-        int noOfCoins = 1;
-        double probability = coinToss.getChanceToNotGetTail(noOfCoins);
-        Assert.assertEquals(0.5, probability, 0.0);
-    }
-
-    @Test
-    public void getChanceToGetTail_should_return_the_probability_to_get_tails_for_2_coins() {
-        Chance coinToss = new Chance();
-        int noOfCoins = 2;
-        double probability = coinToss.getChanceToGetTail(noOfCoins);
-        Assert.assertEquals(0.25, probability, 0.0);
-    }
-
-    @Test
-    public void getChanceTNotoGetTail_should_return_the_probability_to_get_tails_for_2_coins() {
-        Chance coinToss = new Chance();
-        int noOfCoins = 2;
-        double probability = coinToss.getChanceToNotGetTail(noOfCoins);
-        Assert.assertEquals(0.75, probability, 0.0);
+    public void chance_should_take_the_value_between_0_and_1() {
+        Chance chance = Chance.create(0.5);
+        thrown.expect(IllegalArgumentException.class);
+        thrown.expectMessage("The value should be between 0 and 1, but actual is 1.2");
+        Chance.create(1.2);
     }
 }
